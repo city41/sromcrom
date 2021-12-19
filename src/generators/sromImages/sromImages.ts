3c15a08ddb4883320ea3fde31e592cbeead5d6ad
Matt Greer <matt.e.greer@gmail.com>
2021-12-19 11:35:55 -0500

    ok

--- src/generators/sromImages/sromImages.ts
+++ src/generators/sromImages/sromImages.ts
@@ -1,16 +1,24 @@
+import fs from 'fs';
 import path from 'path';
+import ejs from 'ejs';
 import { getCanvasContextFromImagePath } from '../../api/canvas';
 import { extractSromTileSources } from '../../api/srom/extractSromTileSources';
 import { ISROMGenerator, SROMTile, SROMTileSource } from '../../api/srom/types';
-import { Json } from '../../types';
+import { FileToWrite, Json } from '../../types';
 
 type SromImageInput = {
 	name: string;
 	imageFile: string;
 };
 
+type CodeEmit = {
+	template: string;
+	dest: string;
+};
+
 type SromImagesJsonSpec = {
 	inputs: SromImageInput[];
+	codeEmit?: CodeEmit[];
 };
 
 const sromImages: ISROMGenerator = {
@@ -29,7 +37,19 @@ const sromImages: ISROMGenerator = {
 	},
 
 	getSROMSourceFiles(rootDir: string, inputJson: Json, tiles: SROMTile[][][]) {
-		return [];
+		const { codeEmit } = inputJson as SromImagesJsonSpec;
+
+		return (codeEmit ?? []).map<FileToWrite>((codeEmit) => {
+			const templatePath = path.resolve(rootDir, codeEmit.template);
+			const template = fs.readFileSync(templatePath).toString();
+
+			const code = ejs.render(template, { tiles });
+
+			return {
+				path: path.resolve(rootDir, codeEmit.dest),
+				contents: Buffer.from(code),
+			};
+		});
 	},
 };
 

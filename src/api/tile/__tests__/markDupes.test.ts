13ba511e64444b7612a8afa747e4de5b36ff6bf3
Matt Greer <matt.e.greer@gmail.com>
2021-12-19 09:58:46 -0500

    Added jest

---
+++ src/api/tile/__tests__/markDupes.test.ts
@@ -0,0 +1,13 @@
+import { markDupes } from '../markDupes';
+import type { BaseTile } from '../markDupes';
+
+describe('markDupes', function () {
+	it('should mark two tiles as dupes if the comparator returns true', function () {
+		const tiles: BaseTile[] = [{}, {}];
+
+		markDupes(tiles, () => true);
+
+		expect(tiles[0].duplicateOf).toBe(undefined);
+		expect(tiles[1].duplicateOf).toBe(tiles[0]);
+	});
+});
--- src/api/tile/markDupes.ts
+++ src/api/tile/markDupes.ts
@@ -1,11 +1,13 @@
 type TileComparator<T> = (a: T, b: T) => boolean;
 
 type BaseTile = {
-    duplicateOf?: BaseTile;
+	duplicateOf?: BaseTile;
 };
 
-function markDupes<T extends BaseTile>(tiles: T[], comparator: TileComparator<T>) {
-
+function markDupes<T extends BaseTile>(
+	tiles: T[],
+	comparator: TileComparator<T>
+) {
 	tiles.forEach((tile) => {
 		if (tile.duplicateOf) {
 			// tile has already been marked as a dupe, nothing to do
@@ -13,10 +15,7 @@ function markDupes<T extends BaseTile>(tiles: T[], comparator: TileComparator<T>
 		}
 
 		const matchingTiles = tiles.filter((otherTile) => {
-			return (
-				otherTile !== tile &&
-                    comparator(tile, otherTile)
-			);
+			return otherTile !== tile && comparator(tile, otherTile);
 		});
 
 		matchingTiles.forEach((mt) => {
@@ -24,5 +23,6 @@ function markDupes<T extends BaseTile>(tiles: T[], comparator: TileComparator<T>
 		});
 	});
 }
- 
-export { markDupes }
+
+export { markDupes };
+export type { BaseTile };

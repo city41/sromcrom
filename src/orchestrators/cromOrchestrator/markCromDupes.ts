3d3e64b3b5f22d055195ffe94af5d07a06455076
Matt Greer <matt.e.greer@gmail.com>
2024-04-20 21:36:44 -0400

    markCromDupes: any tile involved in an auto animation should not be marked dupe

--- src/orchestrators/cromOrchestrator/markCromDupes.ts
+++ src/orchestrators/cromOrchestrator/markCromDupes.ts
@@ -3,7 +3,16 @@ import { markDupes } from '../../api/tile/markDupes';
 import isEqual from 'lodash/isEqual';
 
 function markCromDupes(tiles: CROMTile[]): void {
-	markDupes(tiles, (a, b) => isEqual(a.cromBinaryData, b.cromBinaryData));
+	markDupes(tiles, (a, b) => {
+		return (
+			isEqual(a.cromBinaryData, b.cromBinaryData) &&
+			// anything involved in an auto animation should be left alone
+			!a.childAnimationFrames &&
+			!a.childOf &&
+			!b.childAnimationFrames &&
+			!b.childOf
+		);
+	});
 }
 
 export { markCromDupes };
